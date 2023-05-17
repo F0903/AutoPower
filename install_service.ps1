3473457b04f06fa9ec15acc402d8c4445e9d9486
Frederik P <frepbh@gmail.com>
2023-05-17 16:34:37 +0200

    updated install script

--- install_service.ps1
+++ install_service.ps1
@@ -12,7 +12,7 @@ $ServiceName = 'AutoPower'
 $Dir = "$($PSScriptRoot)\autopower.exe"
 
 # Some of these are a shot in the dark...
-sc.exe create $ServiceName binPath=$Dir start=auto depend=LanmanServer/LanmanWorkstation/LSM/SysMain/Power/SessionEnv/AarSvc
+sc.exe create $ServiceName binPath=$Dir start=auto depend=LanmanServer/LanmanWorkstation/LSM/SysMain/Power/SessionEnv
 sc.exe start $ServiceName
 Write-Output "`r`nDone!`r`n"
 Pause
