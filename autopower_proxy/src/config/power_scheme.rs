f9721c532f8afe4620251a57c3ae98b9b67ddfa7
Frederik P. <frepbh@gmail.com>
2024-09-12 17:04:01 +0200

    added power saving plan and custom guid support

--- autopower_proxy/src/config/power_scheme.rs
+++ autopower_proxy/src/config/power_scheme.rs
@@ -1,17 +1,24 @@
 use serde::{Deserialize, Serialize};
-use windows::Win32::System::SystemServices::{GUID_MIN_POWER_SAVINGS, GUID_TYPICAL_POWER_SAVINGS};
+use windows::core::GUID;
+use windows::Win32::System::SystemServices::{
+    GUID_MAX_POWER_SAVINGS, GUID_MIN_POWER_SAVINGS, GUID_TYPICAL_POWER_SAVINGS,
+};
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum PowerScheme {
     HighPerformance,
     Balanced,
+    PowerSavings,
+    Custom(String),
 }
 
 impl PowerScheme {
-    pub const fn to_guid(&self) -> windows::core::GUID {
+    pub fn to_guid(&self) -> GUID {
         match self {
             Self::HighPerformance => GUID_MIN_POWER_SAVINGS,
             Self::Balanced => GUID_TYPICAL_POWER_SAVINGS,
+            Self::PowerSavings => GUID_MAX_POWER_SAVINGS,
+            Self::Custom(val) => GUID::from(val.as_str()),
         }
     }
 }
